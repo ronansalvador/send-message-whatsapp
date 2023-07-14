a475851e4d7fc2595c163977b78cc4976ebbb4a2
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-07-13 22:58:16 -0300

    feat: adicionando icon do whats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { BsWhatsapp } from 'react-icons/bs';
 
 export default function Home() {
   const [phone, setPhone] = useState('');
@@ -14,19 +15,25 @@ export default function Home() {
   };
   return (
     <main className='bg-stone-400 h-screen w-screen flex flex-col justify-evenly items-center'>
-      <input
-        type='phone'
-        className='bg-stone-200 rounded-lg p-2 max-sm:w-4/5 w-1/2'
-        onChange={(e) => setPhone(e.target.value)}
-        value={phone}
-        placeholder='5511999999999'
-      />
-      <textarea
-        className='bg-stone-200 rounded-lg p-2 max-sm:w-4/5 max-sm:h-32 w-1/2'
-        onChange={(e) => setMessage(e.target.value)}
-        value={message}
-        placeholder='Mensagem'
-      />
+      <div className='w-screen flex flex-col justify-evenly items-center gap-y-2'>
+        <p>Digite o telefone do destinatário</p>
+        <input
+          type='phone'
+          className='bg-stone-200 rounded-lg p-2 max-sm:w-4/5 w-1/2'
+          onChange={(e) => setPhone(e.target.value)}
+          value={phone}
+          placeholder='5511999999999'
+        />
+      </div>
+      <div className='w-screen flex flex-col justify-evenly items-center gap-y-2'>
+        <p>Digite a sua mensagem</p>
+        <textarea
+          className='bg-stone-200 rounded-lg p-2 max-sm:w-4/5 max-sm:h-32 w-1/2'
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          placeholder='Mensagem'
+        />
+      </div>
       <button
         className='bg-green-800 text-white p-2 rounded-lg hover:scale-125'
         onClick={gerarLink}
@@ -35,7 +42,11 @@ export default function Home() {
       </button>
       <div>
         {link ? (
-          <a className='bg-green-800 text-white p-2 rounded-lg' href={link}>
+          <a
+            className='bg-green-800 text-white p-2 rounded-lg flex justify-center items-center'
+            href={link}
+          >
+            <BsWhatsapp className='mr-2' />
             Clique para enviar
           </a>
         ) : (
