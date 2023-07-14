8772131839b31da8d5cb439922b729658220386a
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-07-13 23:49:38 -0300

    feat: adicionando configuração de favicon no arquivo next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/favicon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
